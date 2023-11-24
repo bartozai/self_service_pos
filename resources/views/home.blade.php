4479a1dd5dc509bb7fe2cbb7b8b26486889abc82
bartozai <berlinzai24@gmail.com>
2023-11-24 15:52:58 +0700

    submmision 2

---
+++ resources/views/home.blade.php
@@ -0,0 +1,12 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>WELCOME</h1>
+    <hr>
+    <a href="">HOME</a> | <a href="">CREATE</a> | <a href="">UPDATE</a> | <a href="">DELETE</a> 
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,31 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view ('welcome');
 });
+
+Route::get('/index', function () {
+    return view('index');
+});
+
+Route::get('/about ', 'About@halaman_about');
+Route::get('/create', 'Create@halaman_create');
+Route::get('/update', 'Update@halaman_update');
+Route::get('/delete', 'Delete@halaman_delete');
+
+Route::get('/home',[App\Httpcontrollers\Homecontroller::class, 'index'])->name('home');
+Route::get('/home', 'Homecontroller@index');
+Route::get('/about', function (){ 
+    $data = [
+    'pageTitle' => 'Tentang Kami',
+    'content' => 'Ini adalah halaman tentang kami.'
+];
+return view('about', $data);
+
+});
+Route::get('/produk','App\Http\controllers\produkcontroller@index');
+
+
+
+
+
